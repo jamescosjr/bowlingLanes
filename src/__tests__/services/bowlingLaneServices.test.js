ae6f4fac14c8ff7d0aae3c291b0b7c77d3f7539e
James Junior <jamescosjr@gmail.com>
2025-03-16 10:31:50 -0300

    add getAllLanesService and corresponding tests; implement retrieval logic for all bowling lanes

--- src/__tests__/services/bowlingLaneServices.test.js
+++ src/__tests__/services/bowlingLaneServices.test.js
@@ -1,8 +1,11 @@
-import { createBowlingLaneService } from '../../domain/services/bowlingLaneService.js';
-import * as bowlingLaneRepository from '../../infrastructure/repositories/bowlingLaneRepositories/bowlingLaneRepositoryWrite.js';
+import { 
+    createBowlingLaneService,
+    getAllLanesService, 
+} from '../../domain/services/bowlingLaneService.js';
 import { connect, closeDatabase, clearDatabase } from '../../../jest/jest.setup.js';
 import { AppError } from '../../domain/erros/customErros.js';
 import { describe, expect, beforeAll, afterEach, afterAll, it, jest } from '@jest/globals';
+
 
 beforeAll(async () => {
     await connect();
@@ -10,7 +13,6 @@ beforeAll(async () => {
 
 afterEach(async () => {
     await clearDatabase();
-    jest.clearAllMocks();
 });
 
 afterAll(async () => {
@@ -36,5 +38,39 @@ describe('createBowlingLaneService', () => {
         const name = '';
 
         await expect(createBowlingLaneService(name)).rejects.toThrowError(AppError);
+   });
+});
+
+describe('getAllLanesService', () => {
+    it('should return all lanes', async () => {
+        await createBowlingLaneService('Lane 1');
+        await createBowlingLaneService('Lane 2');
+
+        const result = await getAllLanesService();
+
+        expect(result).toHaveLength(2);
+        expect(result).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    name: 'Lane 1',
+                    laneSchedule: [],
+                    createdAt: expect.any(Date),
+                    updatedAt: expect.any(Date),
+                }),
+                expect.objectContaining({
+                    name: 'Lane 2',
+                    laneSchedule: [],
+                    createdAt: expect.any(Date),
+                    updatedAt: expect.any(Date),
+                }),
+            ]),
+        );
+    });
+
+    it('should return an empty array if there are no lanes', async () => {
+
+        const result = await getAllLanesService();
+
+        expect(result).toHaveLength(0);
     });
 });
--- src/domain/services/bowlingLaneService.js
+++ src/domain/services/bowlingLaneService.js
@@ -1,5 +1,6 @@
 import { createBowlingLane } from "../../infrastructure/repositories/bowlingLaneRepositories/bowlingLaneRepositoryWrite.js";
 import { AppError } from "../erros/customErros.js";
+import { getAllLanes } from "../../infrastructure/repositories/bowlingLaneRepositories/bowlingLaneRepositoryRead.js";
 
 export async function createBowlingLaneService(name) {
     try {
@@ -7,5 +8,13 @@ export async function createBowlingLaneService(name) {
         return await createBowlingLane(name, laneSchedule);
     } catch (error) {
         throw new AppError('Failed to create bowling lane', error);
+    }
+}
+
+export async function getAllLanesService() {
+    try {
+        return await getAllLanes();
+    } catch (error) {
+        throw new AppError('Failed to get all lanes', error);
     }
 }
