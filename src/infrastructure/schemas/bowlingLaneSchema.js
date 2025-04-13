1bd28e0c1f7718241d24fa242926669481beb99a
jamescosjr <jamescosjr@gmail.com>
2025-04-13 14:33:30 -0300

    refactor: simplify laneSchedule schema by extracting it into a separate definition

--- src/infrastructure/schemas/bowlingLaneSchema.js
+++ src/infrastructure/schemas/bowlingLaneSchema.js
@@ -2,32 +2,22 @@ const mongoose = require('mongoose');
 
 const collectionName = 'bowlingLanes';
 
+const laneScheduleSchema = new mongoose.Schema({
+    date: { type: Date, required: true },
+    startHour: { type: String, required: true },
+    endHour: { type: String, required: true },
+});
+
 const bowlingLaneSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
     },
     laneSchedule: {
-        type: [
-            {
-                _id: false,
-                date: {
-                    type: Date,
-                    required: true,
-                },
-                startHour: {
-                    type: String,
-                    required: true,
-                },
-                endHour: {
-                    type: String,
-                    required: true,
-                },
-            },
-        ],
-        required: true,
-        default: [],
-    },
+        type: [laneScheduleSchema],
+        default: []
+    }
+
 }, { timestamps: true });
 
 const BowlingLane = mongoose.model(collectionName, bowlingLaneSchema);
