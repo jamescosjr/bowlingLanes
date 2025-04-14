6e8eb27f0655fdb1f6d488f993f2e44eee19cc44
jamescosjr <jamescosjr@gmail.com>
2025-04-14 07:38:49 -0300

    fix: update route for getLaneByNameController and add schedule route

--- src/application/controllers/routes.js
+++ src/application/controllers/routes.js
@@ -3,12 +3,14 @@ const {
     createBowlingLaneController,
     getAllLanesController,
     getLaneByNameController,
+    getLanesByScheduleController,
  } = require('./bowlingLaneController.js');
 
 const routes = express.Router();
 
 routes.post('/bowling-lanes', createBowlingLaneController);
 routes.get('/bowling-lanes', getAllLanesController);
-routes.get('/bowling-lanes/:name', getLaneByNameController);
+routes.get('/bowling-lanes/name/:name', getLaneByNameController);
+routes.get('/bowling-lanes/schedule', getLanesByScheduleController);
 
 module.exports = routes;
