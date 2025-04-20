9618e0048fec1faab900de50b6add913ec3d09e4
jamescosjr <jamescosjr@gmail.com>
2025-04-20 14:32:26 -0300

    test: add unit tests for getAllClients function

---
+++ src/__tests__/repositories/client/getAllClients.test.js
@@ -0,0 +1,53 @@
+const {
+    getAllClients,
+} = require('../../../infrastructure/repositories/clientRepositories/clientRepositoryRead.js');
+const { connect, closeDatabase, clearDatabase } = require('../../../../jest/jest.setup');
+const { createClient } = require('../../../infrastructure/repositories/clientRepositories/clientRepositoryWrite.js')
+
+beforeAll(async () => {
+    await connect();
+});
+
+afterEach(async () => {
+    await clearDatabase();
+});
+
+afterAll(async () => {
+    await closeDatabase();
+});
+
+describe('getAllClients', () => {
+    it ('should return all Clients', async () => {
+        const name = 'John Doe';
+        const clientSchedule = [];
+        const documentId = '123456789';
+        const age = 30;
+
+        const result = await createClient(name, clientSchedule, documentId, age);
+
+        expect(result.toObject()).toMatchObject({
+            name,
+            clientSchedule,
+            documentId,
+            age,
+            createdAt: expect.any(Date),
+            updatedAt: expect.any(Date),
+        });
+
+        const allClients = await getAllClients();
+        expect(allClients).toHaveLength(1);
+        expect(allClients[0].toObject()).toMatchObject({
+            name,
+            clientSchedule,
+            documentId,
+            age,
+            createdAt: expect.any(Date),
+            updatedAt: expect.any(Date),
+        });
+    });
+
+    it ('should return an empty array if there are no clients', async () => {
+        const allClients = await getAllClients();
+        expect(allClients).toHaveLength(0);
+    });
+})
