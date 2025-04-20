0d5fb47cc8807b2919d7e0c9ba7e2ceba02c36f8
jamescosjr <jamescosjr@gmail.com>
2025-04-20 10:01:29 -0300

    feat: add route for updating bowling lane details by ID

--- src/application/controllers/routes.js
+++ src/application/controllers/routes.js
@@ -4,6 +4,7 @@ const {
     getAllLanesController,
     getLaneByNameController,
     getLanesByScheduleController,
+    updateLaneByIdController,
  } = require('./bowlingLaneController.js');
 
 const routes = express.Router();
@@ -12,5 +13,6 @@ routes.post('/bowling-lanes', createBowlingLaneController);
 routes.get('/bowling-lanes', getAllLanesController);
 routes.get('/bowling-lanes/name/:name', getLaneByNameController);
 routes.get('/bowling-lanes/schedule', getLanesByScheduleController);
+routes.put('/bowling-lanes/:id', updateLaneByIdController);
 
 module.exports = routes;
