64bde5489fd543c2908de53ee4d2d74d2b706d56
jamescosjr <jamescosjr@gmail.com>
2025-04-20 13:58:22 -0300

    feat: implement createClientController for handling client creation requests

---
+++ src/application/controllers/clientController.js
@@ -0,0 +1,24 @@
+const {
+    createClientService,
+} = require('../../domain/services/clientService.js');
+const { ValidationError } = require("../../domain/erros/customErros.js");
+const { validateClient } = require('../../domain/utils/validations.js');
+
+
+async function createClientController(req, res, next) {
+    try {
+        const { name, documentId, age } = req.body;
+        const validation = validateClient(name, documentId, age);
+        if (!validation.valid) {
+            return next(new ValidationError(validation.message));
+        }
+        const result = await createClientService(name, documentId, age);
+        res.status(201).json(result);
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports = {
+    createClientController,
+}
