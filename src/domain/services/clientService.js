0203e68a548656a84b99b6f01658b17b62c7a3f0
jamescosjr <jamescosjr@gmail.com>
2025-04-20 15:11:05 -0300

    feat: add getAllClientsService for retrieving clients with pagination

--- src/domain/services/clientService.js
+++ src/domain/services/clientService.js
@@ -1,6 +1,12 @@
 const {
     createClient,
 } = require('../../infrastructure/repositories/clientRepositories/clientRepositoryWrite.js');
+const { AppError } = require('../erros/customErros.js');
+const {
+    getAllClients,
+} = require('../../infrastructure/repositories/clientRepositories/clientRepositoryRead.js');
+const { NotFoundError } = require('../erros/customErros.js');
+
 
 async function createClientService (name, documentId, age) {
     const clientSchedule = [];
@@ -11,6 +17,16 @@ async function createClientService (name, documentId, age) {
     }
 }
 
+async function getAllClientsService(filter = {}, page = 1, limit = 10) {
+    try {
+        const clients = await getAllClients(filter, page, limit);
+        return clients;
+    } catch (error) {
+        throw new AppError('Failed to get all clients', error);
+    }
+}
+
 module.exports = {
     createClientService,
+    getAllClientsService,
 };
