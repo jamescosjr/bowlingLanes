cfc24d92fd011690eaa3e8a4b3ba463f9edc1292
jamescosjr <jamescosjr@gmail.com>
2025-04-21 17:12:53 -0300

    feat: implement createScheduleService function for scheduling management

---
+++ src/domain/services/scheduleService.js
@@ -0,0 +1,71 @@
+const { createSchedule } = require("../../infrastructure/repositories/scheduleRepositories/scheduleRepositoryWrite.js");
+const { getLaneById } = require("../../infrastructure/repositories/bowlingLaneRepositories/bowlingLaneRepositoryRead.js");
+const { getClientById } = require("../../infrastructure/repositories/clientRepositories/clientRepositoryRead.js");
+const { AppError, NotFoundError } = require("../erros/customErros.js");
+const { normalizeDate,
+    integerToStringHour,
+} = require("../utils/dates.js");
+const { addScheduleOnClient } = require("../../infrastructure/repositories/clientRepositories/clientRepositoryWrite.js");
+const { addScheduleOnLane } = require("../../infrastructure/repositories/bowlingLaneRepositories/bowlingLaneRepositoryWrite.js");
+const { ObjectId } = require("mongodb");
+
+async function createScheduleService(date, startHour, bowlingLaneId, clientId) {
+    try {
+    const normalizedDate = normalizeDate(date);
+    const normalizedStartHour = integerToStringHour(startHour);
+    const endHour = integerToStringHour(startHour + 2);
+
+    const lane = await getLaneById(bowlingLaneId);
+
+    if (!lane) {
+        throw new NotFoundError("Lane not found");
+    }
+
+    const laneName = lane.name;
+
+    const client = await getClientById(clientId);
+
+    if (!client) {
+        throw new NotFoundError("Client not found");
+    }
+
+    const clientName = client.name;
+    const documentId = client.documentId;
+
+
+    const clientSchedule = {
+        date: normalizedDate,
+        startHour: normalizedStartHour,
+        endHour,
+        bowlingLane: laneName,
+        
+    }
+
+    await addScheduleOnClient(documentId, clientSchedule );
+
+
+    const laneSchedule = {
+        date: normalizedDate,
+        startHour: normalizedStartHour,
+        endHour,
+        client: clientName,
+    }
+
+    await addScheduleOnLane(laneName, laneSchedule);
+
+        return await createSchedule(
+            normalizedDate,
+            normalizedStartHour,
+            endHour,
+            clientId,
+            bowlingLaneId
+        );
+    } catch (error) {
+        throw new AppError("Failed to create schedule", error);
+    }
+
+}
+
+module.exports = {
+    createScheduleService,
+};
