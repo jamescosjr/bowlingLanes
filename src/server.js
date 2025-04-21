073a599d21d1b8fc14ae2f3a85cc092cc5e04877
jamescosjr <jamescosjr@gmail.com>
2025-04-21 17:58:05 -0300

    chore: remove unused imports and commented code from server.js

--- src/server.js
+++ src/server.js
@@ -3,15 +3,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./application/controllers/routes.js");
 const errorHandler = require("./middleware/errorHandler.js");
-// const { validate } = require("express-jsonschema");
-// const yaml = require("js-yaml");
-// const fs = require("fs");
-// const path = require("path");
-// const { fileURLToPath } = ("url");
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// const schema = yaml.load(fs.readFileSync(path.join(__dirname, "./contracts/contract.yaml"), "utf8"));
 
 const app = express();
 const PORT = process.env.PORT;
@@ -29,15 +20,6 @@ if (process.env.NODE_ENV !== "test") {
     console.error("Error connecting to MongoDB:", err);
   });
 }
-
-// const validateSchema = (req, res, next) => {
-//   const validation = validate({
-//     body: schema,
-//   });
-//   validation(req, res, next);
-// }
-
-// app.use(validateSchema);
 
 app.use(routes);
 app.use(errorHandler);
