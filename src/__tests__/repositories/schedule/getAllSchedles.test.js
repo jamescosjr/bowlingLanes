eb7cab5ecd98870229cba58b892b0641966cbdae
jamescosjr <jamescosjr@gmail.com>
2025-04-21 17:19:32 -0300

    test: add unit tests for getAllSchedules functionality

---
+++ src/__tests__/repositories/schedule/getAllSchedles.test.js
@@ -0,0 +1,54 @@
+const { getAllSchedules } = require("../../../infrastructure/repositories/scheduleRepositories/scheduleRepositoryRead.js");
+const { connect, closeDatabase, clearDatabase } = require('../../../../jest/jest.setup');
+const { createSchedule } = require('../../../infrastructure/repositories/scheduleRepositories/scheduleRepositoryWrite.js');
+
+beforeAll(async () => {
+    await connect();
+});
+
+afterEach(async () => {
+    await clearDatabase();
+});
+
+afterAll(async () => {
+    await closeDatabase();
+});
+
+describe('getAllSchedules', () => {
+    it ('should return all schedules', async () => {
+        const date = new Date(2023, 10, 1);
+        const startHour = '16:00';
+        const endHour = '18:00';
+        const bowlingLaneId = 'Lane 1';
+        const clientId = 'Client 1';
+
+        const schedule = await createSchedule(date, startHour, endHour, bowlingLaneId, clientId);
+
+        expect(schedule.toObject()).toMatchObject({
+            date,
+            startHour,
+            endHour,
+            bowlingLaneId,
+            clientId,
+            createdAt: expect.any(Date),
+            updatedAt: expect.any(Date),
+        });
+
+        const allSchedules = await getAllSchedules();
+        expect(allSchedules).toHaveLength(1);
+        expect(allSchedules[0].toObject()).toMatchObject({
+            date,
+            startHour,
+            endHour,
+            bowlingLaneId,
+            clientId,
+            createdAt: expect.any(Date),
+            updatedAt: expect.any(Date),
+        });
+    });
+
+    it ('should return an empty array if there are no schedules', async () => {
+        const allSchedules = await getAllSchedules();
+        expect(allSchedules).toHaveLength(0);
+    });
+});
