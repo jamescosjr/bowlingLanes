09d31cda744759058898733453b7654972ccd04d
jamescosjr <jamescosjr@gmail.com>
2025-04-21 05:06:16 -0300

    feat: add getClientByDocumentId function to retrieve a client by document ID

--- src/infrastructure/repositories/clientRepositories/clientRepositoryRead.js
+++ src/infrastructure/repositories/clientRepositories/clientRepositoryRead.js
@@ -22,7 +22,20 @@ async function getClientById(id) {
     }
 }
 
+async function getClientByDocumentId(documentId) {
+    try {
+        const result = await Client.findOne({ documentId });
+        if (!result) {
+            throw new NotFoundError('Client not found');
+        }
+        return result;
+    } catch (error) {
+        throw new AppError('Failed to get client by document ID');
+    }
+}
+
 module.exports = {
     getAllClients,
     getClientById,
+    getClientByDocumentId,
 }
