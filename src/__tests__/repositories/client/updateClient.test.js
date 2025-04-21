1da170395588b0bd86f4aa2a0e342102197dcd59
jamescosjr <jamescosjr@gmail.com>
2025-04-21 05:50:25 -0300

    test: add unit tests for updating client schedules in updateClient.test.js

---
+++ src/__tests__/repositories/client/updateClient.test.js
@@ -0,0 +1,94 @@
+const { AppError } = require('../../../domain/erros/customErros.js');
+const { 
+    createClient, 
+    addScheduleOnClient, 
+    removeScheduleOnClient, 
+} = require('../../../infrastructure/repositories/clientRepositories/clientRepositoryWrite.js');
+const { getClientByDocumentId } = require('../../../infrastructure/repositories/clientRepositories/clientRepositoryRead.js');
+const { ObjectId } = require('mongodb');
+const  { connect, closeDatabase, clearDatabase } = require('../../../../jest/jest.setup');
+
+beforeAll(async () => {
+    await connect();
+});
+
+afterEach(async () => {
+    await clearDatabase();
+});
+
+afterAll(async () => {
+    await closeDatabase();
+});
+
+describe('updates', () => {
+    describe('addScheduleOnClient and removeScheduleOnClient', () => {
+        it ('should update a client with the given name and schedule', async () => {
+            const name = 'John Doe';
+            const clientSchedule = [];
+            const documentId = '123456789';
+            const age = 30;
+
+            await createClient(name, clientSchedule, documentId, age);
+
+            const updatedClientSchedule1 =
+                {
+                    date: new Date(2025, 7, 12),
+                    startHour: '10:00',
+                    endHour: '12:00',
+                    bowlingLane: 'Lane 1',
+                }
+
+            const updatedClientSchedule2 =
+                {
+                    date: new Date(2025, 7, 12),
+                    startHour: '12:00',
+                    endHour: '14:00',
+                    bowlingLane: 'Lane 2',
+                }
+            await addScheduleOnClient(documentId, updatedClientSchedule1);
+            await addScheduleOnClient(documentId, updatedClientSchedule2);
+            const client = await getClientByDocumentId(documentId);
+            expect(client).toMatchObject({
+                _id: expect.any(ObjectId),
+                name: 'John Doe',
+                clientSchedule: [
+                    {
+                        date: new Date(2025, 7, 12),
+                        startHour: '10:00',
+                        endHour: '12:00'
+                    },
+                    {
+                        date: new Date(2025, 7, 12),
+                        startHour: '12:00',
+                        endHour: '14:00'
+                    }
+                ],
+                documentId,
+                age,
+                createdAt: expect.any(Date),
+                updatedAt: expect.any(Date),
+            }); 
+
+            await removeScheduleOnClient(documentId, updatedClientSchedule1);
+
+            const updatedClient = await getClientByDocumentId(documentId);
+
+            expect(updatedClient).toMatchObject({
+                _id: expect.any(ObjectId),
+                name: 'John Doe',
+                clientSchedule: [
+                    {
+                        date: new Date(2025, 7, 12),
+                        startHour: '12:00',
+                        endHour: '14:00'
+                    }
+                ],
+                documentId,
+                age,
+                createdAt: expect.any(Date),
+                updatedAt: expect.any(Date),
+            });
+            expect(updatedClient.clientSchedule.length).toBe(1);
+        });
+    });
+});
