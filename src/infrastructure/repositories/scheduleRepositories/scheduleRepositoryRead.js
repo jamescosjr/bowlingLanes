17fc97748198fdfb4ebd013b8aca24622762f0e7
jamescosjr <jamescosjr@gmail.com>
2025-04-28 04:40:13 -0300

    feat: enhance getAllSchedules function to support filtering, pagination, and limit

--- src/infrastructure/repositories/scheduleRepositories/scheduleRepositoryRead.js
+++ src/infrastructure/repositories/scheduleRepositories/scheduleRepositoryRead.js
@@ -1,9 +1,28 @@
 const Schedule = require('../../schemas/scheduleSchema.js');
 const { AppError } = require("../../../domain/erros/customErros.js");
 
-async function getAllSchedules() {
+async function getAllSchedules(filter, skip, limit) {
     try {
-        const schedules = await Schedule.find();
+        const query = {};
+
+        if (filter.date) {
+            query.date = filter.date;
+        }
+        if (filter.startHour) {
+            query.startHour = filter.startHour;
+        }
+        if (filter.bowlingLaneId) {
+            query.bowlingLaneId = filter.bowlingLaneId;
+        }
+        if (filter.clientId) {
+            query.clientId = filter.clientId;
+        }
+
+        const schedules = await Schedule.find(query)
+            .skip(skip)
+            .limit(limit)
+            .exec();
+
         return schedules;
     } catch (error) {
         throw new AppError('Error fetching schedules', 500);
